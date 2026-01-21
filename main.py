f73a70c95acd28d8995098d0680f8782e2e69105
Vanderlei Burkett <vburkett@purdue.edu>
2026-01-21 10:40:12 -0500

    Added Simple Shape Function

--- main.py
+++ main.py
@@ -14,3 +14,56 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import turtle
+
+from turtle import *
+
+#triangle creation
+teleport(-250,0)
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
+
+#reset forward
+left(120)
+
+#octagon creation
+teleport(-13,0)
+forward(36)
+left(45)
+forward(36)
+left(45)
+forward(36)
+left(45)
+forward(36)
+left(45)
+forward(36)
+left(45)
+forward(36)
+left(45)
+forward(36)
+left(45)
+forward(36)
+
+#reset forward
+left(45)
+
+#hexagon creation
+teleport(175,0)
+forward(50)
+left(60)
+forward(50)
+left(60)
+forward(50)
+left(60)
+forward(50)
+left(60)
+forward(50)
+left(60)
+forward(50)
+left(60)
+
+#stay on screen after drawing
+turtle.done()
