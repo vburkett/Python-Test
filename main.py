44a459c65c06d5e471389eef9fbf17a270166818
Vanderlei Burkett <vburkett@purdue.edu>
2026-02-10 11:49:00 -0500

    Math Testing

--- main.py
+++ main.py
@@ -1,69 +1,16 @@
-# This is a sample Python script.
+import minemath
+a = float(input("Enter a number A for (Ax+B=0): "))
+b = float(input("Enter a number B for (Ax+B=0): "))
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+x = minemath.SolveAxplusB(a,b)
+print("The solution for x is...")
+print(x)
 
+print()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+a = float(input("Enter a value for A in the form (A^2+B^2=C^2): "))
+b = float(input("Enter a value for B in the form (A^2+B^2=C^2): "))
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-import turtle
-
-from turtle import *
-
-#triangle creation
-teleport(-250,0)
-forward(100)
-left(120)
-forward(100)
-left(120)
-forward(100)
-
-#reset forward
-left(120)
-
-#octagon creation
-teleport(-13,0)
-forward(36)
-left(45)
-forward(36)
-left(45)
-forward(36)
-left(45)
-forward(36)
-left(45)
-forward(36)
-left(45)
-forward(36)
-left(45)
-forward(36)
-left(45)
-forward(36)
-
-#reset forward
-left(45)
-
-#hexagon creation
-teleport(175,0)
-forward(50)
-left(60)
-forward(50)
-left(60)
-forward(50)
-left(60)
-forward(50)
-left(60)
-forward(50)
-left(60)
-forward(50)
-left(60)
-
-#stay on screen after drawing
-turtle.done()
+c = minemath.SolveHypotenuse(a,b)
+print("The solution for c is...")
+print(c)
---
+++ minemath.py
@@ -0,0 +1,6 @@
+import math
+def SolveAxplusB(a,b):
+    return -b/a
+
+def SolveHypotenuse(a,b):
+    return math.sqrt(a*a+b*b)
